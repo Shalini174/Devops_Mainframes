c2f4d290436da8870b6b54e6944ab8f2aa364598
Parimala2994 <35544353+Parimala2994@users.noreply.github.com>
2019-07-30 02:32:17 -0400

    Add files via upload

---
+++ SAMPLE2.cbl
@@ -0,0 +1,18 @@
+CBL TEST
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. SAMPLE2.
+      *****************************************************************
+      *                                                               *
+      *   Test program                                                *
+      *                                                               *
+      *****************************************************************
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       LINKAGE SECTION.
+       PROCEDURE DIVISION.
+       0001-MAIN.
+           DISPLAY 'HELLO WORLD!'.
+           GOBACK.
+       END PROGRAM SAMPLE2.
