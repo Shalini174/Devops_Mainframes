db95572d673acb2ae4e1e2b6ad067f4248c4e970
Parimala2994 <35544353+Parimala2994@users.noreply.github.com>
2019-08-19 07:45:33 -0400

    Update SAMPLE2.cbl

--- SAMPLE2.cbl
+++ SAMPLE2.cbl
@@ -3,7 +3,7 @@ CBL TEST
        PROGRAM-ID. SAMPLE2.
       *****************************************************************
       *                                                               *
-      *   This is  Test program                                      *
+      *   This is a Test program                                      *
       *                                                               *
       *****************************************************************
        ENVIRONMENT DIVISION.
